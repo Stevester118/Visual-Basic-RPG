903bb1ab047d823b10bde0f105396bda32627c0e
Stephen Cirabisi <Stevester118@gmail.com>
2018-09-26 01:01:43 -0400

    Initial commit

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim board As Integer(,) = {{1, 1, 1, 1, 1}, _
+                                   {1, 0, 0, 0, 1}, _
+                                   {1, 0, 2, 0, 1}, _
+                                   {1, 1, 1, 1, 1}}
+        Game1.GameBoard = board
+    End Sub
+End Class
---
+++ Game.vb
@@ -0,0 +1,32 @@
+﻿Public Class Game
+    Private gb As Integer(,) = {}
+    Public Property GameBoard As Integer(,)
+        Get
+            Return gb
+        End Get
+        Set(value As Integer(,))
+            gb = value
+            Refresh()
+        End Set
+    End Property
+
+
+    Private Sub Game_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Game_Paint(sender As Object, e As System.Windows.Forms.PaintEventArgs) Handles Me.Paint
+        'the paint event is when the control is drawn
+        'you use the graphics class here to draw things on here
+        e.Graphics.FillRectangle(Brushes.White, New Rectangle(0, 0, Me.Width, Me.Height))
+        If GameBoard.GetUpperBound(0) >= 0 And GameBoard.GetUpperBound(1) >= 0 Then
+            For x = 0 To GameBoard.GetUpperBound(0)
+                For y = 0 To GameBoard.GetUpperBound(1)
+                    If GameBoard(x, y) = 1 Then
+                        e.Graphics.DrawImage(My.Resources.Tree1)
+                    End If
+                Next
+            Next
+        End If
+    End Sub
+End Class
